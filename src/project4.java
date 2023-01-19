c8e1d14320ba991251b10cf5e07fb59bf1addc0a
pjinesh <pjinesh.cad@gmail.com>
2023-01-19 17:12:10 +0530

    commit and pushing after creating source folder

---
+++ src/project4.java
@@ -0,0 +1,10 @@
+public class project4 {
+
+    public static void main(String[] args) {
+        System.out.println("  +--------------------------+");
+        System.out.println(" /                            \\ ");
+        System.out.println("|       Hello Jinesh           |");
+        System.out.println(" \\                            /");
+        System.out.println("  +--------------------------+");
+    }
+}
